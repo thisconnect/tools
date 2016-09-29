afb9562994dc8133e9ef7eb27cc3caa1c0a27b8c
thisconnect <hi@this-connect.me>
2016-09-29 14:01:05 +0200

    comments out console.log

--- images/index.js
+++ images/index.js
@@ -5,12 +5,12 @@ const { resize } = require('./size.js')
 module.exports = ({ input, output, width }) => {
   return fstat(output)
   .then(stat => stat.isFile())
-  .then(() => console.log('file exists', output))
+  // .then(() => console.log('file exists', output))
   .catch(err => {
     return readFile(input)
     .then(buffer => !!width ? resize(buffer, { width }) : buffer)
 		.then(buffer => compress(buffer))
 		.then(buffer => writeFile(output, buffer))
-		.then(() => console.log('new file', output))
+		// .then(() => console.log('new file', output))
   })
 }
