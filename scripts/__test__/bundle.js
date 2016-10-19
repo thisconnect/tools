4903c395f475401c2ba0967644130d83fe43475d
thisconnect <hi@this-connect.me>
2016-10-19 16:29:53 +0200

    changes api to src and dest

--- scripts/__test__/bundle.js
+++ scripts/__test__/bundle.js
@@ -10,16 +10,16 @@ test('cleanup', t => {
 })
 
 test('bundle es6 modules', t => {
-  const entry = resolve(__dirname, 'fixtures/entry.js')
+  const src = resolve(__dirname, 'fixtures/entry.js')
   const dest = resolve(__dirname, 'build/bundle/bundle.js')
 
-  bundle({ entry, dest })
+  bundle({ src, dest })
   .then(() => readFile(dest))
   .then(code => t.ok(Buffer.byteLength(code) > 1, 'has some length'))
   .then(() => readFile(dest + '.map', { encoding: 'utf8' }))
   .then(map => JSON.parse(map))
   .then(({ version, sources }) => {
-    const relSrcPath = relative(dirname(dest), entry)
+    const relSrcPath = relative(dirname(dest), src)
     t.ok(version >= 3, 'sourcemap version >= 3')
     t.ok(sources.indexOf(relSrcPath) > -1, `sources contain ${relSrcPath}`)
   })
