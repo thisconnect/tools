238ee386670b50e79917cacf622689005535fc7f
thisconnect <hi@this-connect.me>
2016-10-19 16:29:05 +0200

    unfifies module and adds libs and write options

--- scripts/bundle.js
+++ scripts/bundle.js
@@ -1,89 +1,86 @@
 const rollup = require('rollup').rollup
-const nodeResolve = require('rollup-plugin-node-resolve')
-const commonjs = require('rollup-plugin-commonjs')
-// const json = require('rollup-plugin-json')
-const babel = require('rollup-plugin-babel')
-const replace = require('rollup-plugin-replace')
-const uglify = require('rollup-plugin-uglify')
-const filesize = require('rollup-plugin-filesize')
+const pluginNPM = require('rollup-plugin-node-resolve')
+const pluginCommonjs = require('rollup-plugin-commonjs')
+// const pluginJson = require('rollup-plugin-json')
+const pluginBabel = require('rollup-plugin-babel')
+const pluginReplace = require('rollup-plugin-replace')
+const pluginUglify = require('rollup-plugin-uglify')
+const pluginFilesize = require('rollup-plugin-filesize')
 // const builtins = require('rollup-plugin-node-builtins')
 
 const env = process.env.NODE_ENV || 'production'
 
-module.exports = ({ entry, dest, format = 'iife' }) => {
-  return rollup({
-    entry,
-    // external: ['date-fns/format', 'marked', 'react', 'react-dom'],
-    plugins: [
-      replace({
-        'process.env.NODE_ENV': JSON.stringify(env)
-      }),
-      babel({
-        babelrc: false,
-        presets: ['es2015-rollup' , 'react', 'stage-3']
-      }),
-      uglify(),
-      filesize({
-        format: {
-          // base: 10,
-          exponent: 0
-          // standard: 'iec',
-          // bits: true
-        }
-      })
-    ]
-  })
-  .then(bundle => bundle.write({
-    banner: '// App',
-    dest,
-    format,
-    /*globals: {
-      'date-fns/format': 'format',
-      'marked': 'marked',
-      'react': 'React',
-      'react-dom': 'ReactDOM'
-    },*/
-    indent: false,
-    sourceMap: true
-  }))
-}
+const replace = pluginReplace({
+  'process.env.NODE_ENV': JSON.stringify(env)
+})
 
-module.exports.libs = ({ entry, dest, format = 'iife' }) => {
+const npm = pluginNPM({
+  jsnext: true
+})
+
+const commonjs = pluginCommonjs({
+  include: 'node_modules/**',
+  namedExports: {
+    'node_modules/react/react.js': ['Component', 'createElement']
+  }
+})
+
+const babel = pluginBabel({
+  babelrc: false,
+  // exclude: 'node_modules/**',
+  presets: ['react', 'stage-3', 'es2015-rollup']
+})
+
+const uglify = pluginUglify({
+  compress: {
+    screw_ie8: true,
+    warnings: false
+  }
+})
+
+const filesize = pluginFilesize({
+  format: {
+    // base: 10,
+    exponent: 0
+    // standard: 'iec',
+    // bits: true
+  }
+})
+
+module.exports = ({
+  src, dest,
+  format = 'iife',
+  write = true,
+  libs = false
+}) => {
+
+  const plugins = libs ? [
+    replace, npm, commonjs, babel, uglify, filesize
+  ] : [
+    replace, babel, uglify, filesize
+  ]
+
   return rollup({
-    entry,
-    plugins: [
-      replace({
-        'process.env.NODE_ENV': JSON.stringify(env)
-      }),
-      nodeResolve({
-        jsnext: true
-      }),
-      commonjs({
-        include: 'node_modules/**',
-        namedExports: {
-          'node_modules/react/react.js': ['Component', 'createElement']
-        }
-      }),
-      // json(),
-      babel({
-        babelrc: false,
-        // exclude: 'node_modules/**',
-        presets: ['react', 'stage-3', 'es2015-rollup']
-      }),
-      uglify({
-        compress: {
-          screw_ie8: false,
-          warnings: false
-        }
-      }),
-      filesize()
-    ]
+    entry: src,
+    // external: ['date-fns/format', 'marked', 'react', 'react-dom'],
+    plugins
   })
-  .then(bundle => bundle.write({
-    banner: '// Libs',
-    dest,
-    format,
-    indent: false,
-    sourceMap: true
-  }))
+  .then(bundle => {
+    if (!write){
+      return bundle.generate({ format, sourceMap: 'inline' })
+    }
+    return bundle.write({
+      banner: '// App',
+      dest,
+      format,
+      // globals: {
+      //   'date-fns/format': 'format',
+      //   'marked': 'marked',
+      //   'react': 'React',
+      //   'react-dom': 'ReactDOM'
+      // },
+      indent: false,
+      sourceMap: true
+    })
+  })
 }
