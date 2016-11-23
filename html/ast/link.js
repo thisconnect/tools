9cbb4c3c5d982afd9f4893f605174bf656d32ebc
thisconnect <hi@this-connect.me>
2016-11-23 12:56:15 +0100

    inline minified css or bundle src file

--- html/ast/link.js
+++ html/ast/link.js
@@ -1,22 +1,20 @@
-const { basename, dirname, resolve } = require('path')
+const { basename, dirname, extname, resolve } = require('path')
 const { readFile } = require('fildes')
 const { search, getHref } = require('./index.js')
 const { appendFragment, linkToStyle } = require('./modify.js')
-const minify = require('../../styles/index.js')
+const bundle = require('../../styles/index.js')
 const { log } = require('../../log/index.js')
 
 const append = (node, path, { src, dest }) => {
   const dir = dirname(path)
-  // const ext = extname(path)
-  // const base = basename(path, ext)
-  // let file = resolve(dest, dir, `${base}.min${ext}`)
-  const base = basename(path)
-  let file = resolve(dest, dir, base)
+  const ext = extname(path)
+  const base = basename(path, ext)
+  let file = resolve(dest, dir, base + ext)
+  let filemin = resolve(dest, dir, `${base}.min${ext}`)
 
-  return readFile(file)
+  return readFile(filemin)
   .catch(() => {
-
-    return minify({
+    return bundle({
       src: resolve(src, path),
       dest: file,
       assets: './'
@@ -25,17 +23,7 @@ const append = (node, path, { src, dest }) => {
       file = resolve(src, path)
       return css
     })
-  /*  return readFile(file, { encoding: 'utf8' })
-    .then(css => {
-      console.log(file)
-      console.log(resolve(dest, ))
-      console.log(css)
-      //
-      // { input, output, assets }
-      return file
-    })
-    */
-  }) // TODO MINIFY CSS
+  })
   .then(data => data.toString().replace(/\/\*#\ssourceMappingURL\=.*$/, ''))
   .then(css => appendFragment(node, css))
   .then(() => file)
