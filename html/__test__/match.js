7fff7c0acdc3ed801fe046d9493f06e9391f23e4
thisconnect <hi@this-connect.me>
2017-01-30 12:53:15 +0100

    adds getNodesByTagName and getNodeById

---
+++ html/__test__/match.js
@@ -0,0 +1,72 @@
+const test = require('tape')
+const { parse } = require('parse5')
+const { getNodeById, getNodesByTagName } = require('../ast/match.js')
+
+test('getNodesByTagName', t => {
+  const doc = parse('<!doctype html><div><p><span></span><span></span></p></div>')
+  getNodesByTagName(doc, 'span')
+  .then(nodes => {
+    t.ok(Array.isArray(nodes), 'returns an array')
+    t.equal(nodes.length, 2, 'has 2 nodes')
+    t.equal(nodes[0].tagName, 'span', 'element is a span')
+    t.end()
+  })
+  .catch(err => t.fail(err))
+})
+
+test('getNodeById', t => {
+  const doc = parse('<!doctype html><div><p><span id=foo></span><span></span></p></div>')
+  getNodeById(doc, 'foo')
+  .then(node => {
+    t.equal(typeof node, 'object', 'is object')
+    t.equal(node.tagName, 'span', 'element is a span')
+    t.equal(node.attrs[0].value, 'foo', 'first attribute has value foo')
+    t.end()
+  })
+  .catch(err => t.fail(err))
+})
+
+/*
+test('match by tagName', t => {
+  const doc = parse('<!doctype html><span></span><div><p><a href="#"></a></p></div>')
+  const html = doc.childNodes[1]
+  const body = html.childNodes[1]
+
+  match(body, 'a')
+  .then(ast => {
+    t.equal(typeof ast, 'object', 'is object')
+    t.end()
+
+    return;
+
+  })
+  .catch(err => t.fail(err))
+})
+
+test('match descendant', t => {
+  const doc = parse('<!doctype html><div><p></p><p></p></div>')
+  const html = doc.childNodes[1]
+  const body = html.childNodes[1]
+
+  match(body, 'div p')
+  .then(nodes => {
+    console.log('found', nodes)
+    t.equal(typeof nodes, 'object', 'is object')
+    t.end()
+  })
+  .catch(err => t.fail(err))
+})
+
+test('match by classNames', t => {
+  const doc = parse('<!doctype html><div class="foo bar"></div>')
+  const html = doc.childNodes[1]
+  const body = html.childNodes[1]
+
+  match(body, '.foo.bar')
+  .then(ast => {
+    t.equal(typeof ast, 'object', 'is object')
+    t.end()
+  })
+  .catch(err => t.fail(err))
+})
+*/
---
+++ html/ast/match.js
@@ -0,0 +1,90 @@
+// const parse = require('css-what')
+
+exports.getNodesByTagName = (nodes, tagName) => {
+  return new Promise((resolve) => {
+    const results = []
+    const que = getChildNodes(nodes)
+    for (const node of que){
+      const { childNodes } = node
+      if (node.tagName === tagName){
+        results.push(node)
+      }
+      if (childNodes && childNodes.length){
+        que.push(...childNodes)
+      }
+    }
+    resolve(results)
+  })
+
+}
+
+exports.getNodeById = (nodes, id) => {
+  return new Promise((resolve) => {
+    const que = getChildNodes(nodes)
+    for (const node of que){
+      const { attrs, childNodes } = node
+      if (attrs && attrs.length){
+        for (const attr of attrs){
+          if (attr.name === 'id' && attr.value === id){
+            return resolve(node)
+          }
+        }
+      }
+      if (childNodes && childNodes.length){
+        que.push(...childNodes)
+      }
+    }
+    resolve(null)
+  })
+}
+
+function getChildNodes(nodes){
+  const childNodes = nodes.childNodes
+  return (childNodes && childNodes.length) ? [...childNodes] : [...nodes]
+}
+
+/*
+function match(node, t){
+  const tokens = t.reverse()
+  console.log('---tokens--', tokens)
+
+  for (const token of tokens){
+    if (token.type === 'tag'){
+      if (node.nodeName !== token.name){
+        return false
+      }
+    } else if (token.type === 'descendant'){
+
+    }
+  }
+  return true
+}
+
+*/
+
+
+
+/*
+// querySelectorAll
+exports.querySelectorAll = (nodes, selector) => {
+  return new Promise((resolve, reject) => {
+    const selectors = parse(selector)
+
+    const results = []
+    const que = (nodes.childNodes && nodes.childNodes.length) ? [...nodes.childNodes] : [...nodes]
+    for (const node of que){
+      for (const tokens of selectors){
+        if (match(node, tokens)){
+          results.push(node)
+        }
+      }
+
+      if (node.childNodes.length){
+        que.push(...node.childNodes)
+      }
+    }
+    resolve(results)
+
+  })
+}
+*/
