caa5ab5cfab14d721573972afb466d567cf13629
thisconnect <hi@this-connect.me>
2018-01-15 10:04:39 +0100

    removes karma

--- test/__test__/runner.js
+++ test/__test__/runner.js
@@ -1,25 +1,25 @@
 const test = require('tape')
-const runner = require('../runner.js')
+// const runner = require('../runner.js')
 
 const { TRAVIS, TRAVIS_OS_NAME } = process.env
 
-test('test karma runner', t => {
+test.skip('test karma runner', t => {
 
-  if (TRAVIS && TRAVIS_OS_NAME == 'osx'){
-    t.comment('karma currently doesnt work on osx-travis')
-    return t.end()
-  }
-
-  runner({
-    basePath: __dirname,
-    files: [
-      // 'fixtures/*.html',
-      // 'fixtures/*.css',
-      'fixtures/*.karma.js'
-    ]
-  })
-  // .then(exitCode => console.log())
-  .then(exitCode => t.equal(exitCode, 0, 'exitCode is 0'))
-  .then(() => t.end())
-  .catch(t.end)
+  // if (TRAVIS && TRAVIS_OS_NAME == 'osx'){
+  //   t.comment('karma currently doesnt work on osx-travis')
+  //   return t.end()
+  // }
+  //
+  // runner({
+  //   basePath: __dirname,
+  //   files: [
+  //     // 'fixtures/*.html',
+  //     // 'fixtures/*.css',
+  //     'fixtures/*.karma.js'
+  //   ]
+  // })
+  // // .then(exitCode => console.log())
+  // .then(exitCode => t.equal(exitCode, 0, 'exitCode is 0'))
+  // .then(() => t.end())
+  // .catch(t.end)
 })
